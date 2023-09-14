27dc954e6efc4b0fb7760b957622f32e9b681055
Youssef Salem <101949937+Youssefsalem3@users.noreply.github.com>
2023-09-14 06:06:12 +0300

    Update Loanmodel.py

--- Loanmodel.py
+++ Loanmodel.py
@@ -4,8 +4,9 @@ from flask import Flask, request, jsonify
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-# Load the Logistic Regression model from the joblib file
+
 loaded_model = joblib.load('C:\\Users\\20109\\Downloads\\loan_model.joblib2')
+
 # Create a Flask app
 app = Flask(__name__)
 
@@ -16,7 +17,6 @@ def predict():
         # Get data from the request
         data = request.get_json()
 
-        # Define a mapping for categorical to numerical values
         categorical_mapping = {
             'gender': {'male': 0, 'female': 1},
             'married': {'no': 0, 'yes': 1},
@@ -31,22 +31,17 @@ def predict():
             if column in data:
                 data[column] = mapping[data[column]]
 
-        # Convert data to a format expected by the model
+        
         data_array = [data['gender'], data['married'], data['dependents'], data['education'], data['self_employed'],
                       data['applicant_income'], data['coapplicant_income'], data['loanamount'], data['loan_amount_term'],
                       data['credit_history'], data['property_area']]
         data_array = [float(x) for x in data_array]  # Ensure data types are appropriate for your model
-        # Convert data_array to a 1D numpy array
-
-      # Make predictions using the loaded model
-        data_array = np.array(data_array).reshape(1, -1)  # Reshape data to 2D
+        
+        data_array = np.array(data_array).reshape(1, -1)
         probabilities = loaded_model.predict_proba(data_array)
 
-        # Convert the model's output to a percentage (probability of class 1)
         percentage = probabilities[0][1] * 100
 
-
-        # Return the percentage as JSON
         return jsonify({'percentage': percentage})
 
     except Exception as e:
